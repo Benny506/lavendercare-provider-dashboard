48e46d2f1d2731af745cce09fd441039c0a6b5b1
benny506 <olomufeh@gmail.com>
2025-11-04 09:24:41 +0100

    updates

--- src/layouts/IndividualDashboardLayout.jsx
+++ src/layouts/IndividualDashboardLayout.jsx
@@ -6,9 +6,9 @@ import { Outlet } from 'react-router-dom'
 const IndividualDashboardLayout = () => {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className="flex min-h-screen max-h-screen bg-primary-100 bg-img2">
+    <div className="flex w-full h-screen overflow-hidden bg-primary-100 bg-img2">
       <IndividualSidebar isOpen={isOpen} setIsOpen={setIsOpen} />
-      <div className="flex-1 p-6 max-h-screen overflow-y-auto">
+      <div className="flex-1 p-6 overflow-y-auto">
         <IndividualTopBar setIsOpen={setIsOpen} />
         <div className=''>
           <Outlet />
